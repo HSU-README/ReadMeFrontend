67a9f246da4a2b246afbf2b3aceaa55c22d694d2
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2022-03-03 21:01:42 +0900

    make signup page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import {BrowserRouter,Routes,Route } from 'react-router-dom';
+import Signup from 'pages/sign/Signup';
 function App() {
-  return <div className="App">READ ME</div>;
+  return (
+    <div className="App">
+      READ ME
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/signup" element={<Signup/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
