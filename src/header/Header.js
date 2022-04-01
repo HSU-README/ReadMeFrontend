79b3e3c4c6549870cf539afbe404414c919908ad
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2022-04-01 14:16:18 +0900

    추천디자인 수정

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import './Header.css';
 import logo from '../assets/images/logo.jpg';
 import {InputAdornment,Stack, Button} from '@mui/material';
@@ -9,6 +9,8 @@ import SimpleImageSlider from 'react-simple-image-slider';
 import SearchKeyword from './SearchKeyword.js';
 const Header=()=>{
   const [keywordBoxVisible, setKeywordBoxVisible] = useState(false);
+  const [keywordBoxLeft, setkeyWordBoxLeft] = useState("0px");
+  const [keywordBoxTop, setkeyWordBoxTop] = useState("0px");
   const keywordBox={
         position:"absolute",
         border:"2px solid gray",
@@ -16,15 +18,32 @@ const Header=()=>{
         borderRadius:"15px",
         width:"700px",
         height:"150px",
-        zIndex:"1", 
-        left:"37%",
-        top:"15%"
+        zIndex:"1",
+        textAlign:"left", 
+        left:keywordBoxLeft,
+        top:keywordBoxTop
     }
 
+  const keywordTag={
+    
+    backgroundColor: 'lightGray',
+    width: '60px',
+    height: '20px',
+    borderRadius: '5px',
+    textAlign: 'center',
+    marginTop:"20px",
+    marginLeft:"35px",
+    marginRight:"35px"
+  }
+  const keywordBoxRef=useRef(null);
   const change=()=>{
     console.log(keywordBoxVisible)
     setKeywordBoxVisible(true)
   }
+  useEffect(()=>{
+    setkeyWordBoxLeft(`${keywordBoxRef.current.getBoundingClientRect().x}px`)
+    setkeyWordBoxTop(`${keywordBoxRef.current.getBoundingClientRect().y+42}px`)
+  },[keywordBoxRef.current.getBoundingClientRect().x,keywordBoxRef.current.getBoundingClientRect().y])
   useEffect(()=>[
     console.log(keywordBoxVisible)
   ],keywordBoxVisible)
@@ -39,12 +58,14 @@ const Header=()=>{
     const moveHome=()=>{
         window.location.href=""
     }
+   
+
     return (
       <div className="headerMain">
         <div className="inner">
           <img src={logo} className="logo" onClick={moveHome} />
-          <span className="inner">
-            <div style={{ display: 'relative' }} onClick={change}>
+          <span className="inner" id="inner">
+            <div style={{ display: 'relative' }} ref={keywordBoxRef} onClick={change}>
               <SearchKeyword />
             </div>
           </span>
@@ -55,17 +76,30 @@ const Header=()=>{
             회원가입
           </Button>
         </div>
-        {keywordBoxVisible&& 
+        {keywordBoxVisible && (
           <div style={keywordBox}>
             <div style={{ textAlign: 'left', border: '1px solit gray', marginLeft: '10px', marginTop: '8px' }}>
               추천 키워드
             </div>
+
             <hr />
+            <div style={{ display: 'inlineBlock' }}>
+              <div style={keywordTag}>공대</div>
+
+              <div style={keywordTag}>디자인</div>
+            </div>
           </div>
-        }
+        )}
 
         <div style={{ zIndex: '0', position: 'relative' }}>
-          <SimpleImageSlider width="100%" height="300px" images={dummyImages} showBullets={true} showNavs={true}  onClick={()=>setKeywordBoxVisible(false)} />
+          <SimpleImageSlider
+            width="100%"
+            height="300px"
+            images={dummyImages}
+            showBullets={true}
+            showNavs={true}
+            onClick={() => setKeywordBoxVisible(false)}
+          />
         </div>
       </div>
     );
