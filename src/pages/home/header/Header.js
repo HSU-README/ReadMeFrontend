ea4df0f946ef887e0df44ce8e3856741ce712b62
2jaebbang <2jaebbang@naver.com>
2022-05-08 10:50:52 +0900

    fix: 메인페이지 헤더 수정

--- src/pages/home/header/Header.js
+++ src/pages/home/header/Header.js
@@ -12,31 +12,32 @@ const Header = () => {
   const [keywordBoxLeft, setkeyWordBoxLeft] = useState('0px');
   const [keywordBoxTop, setkeyWordBoxTop] = useState('0px');
   //로그인 정보 state
-  const { loginCheck } = useSelector(state => state.loginCheck)
+  const { loginCheck } = useSelector((state) => state.loginCheck);
   //추천검색어 창 나타나게하는 리덕스 전역 관리 변수
-  const {visibleCheck} = useSelector(state=>state.visibleCheck) 
+  const { visibleCheck } = useSelector((state) => state.visibleCheck);
   const location = useLocation();
-  const dispatch = useDispatch()
-  const signIn = ()=>{
-    dispatch({type:'signIn'})
-  }
-  const signOut =()=>{
-    dispatch({type:'signOut'})
-  }
-  const visible=()=>{ //추천검색어를 보이게
-    dispatch({type:'visible'})
-  }
+  const dispatch = useDispatch();
+  const signIn = () => {
+    dispatch({ type: 'signIn' });
+  };
+  const signOut = () => {
+    dispatch({ type: 'signOut' });
+  };
+  const visible = () => {
+    //추천검색어를 보이게
+    dispatch({ type: 'visible' });
+  };
   const keywordBoxLeftRight = {
     left: keywordBoxLeft,
     top: keywordBoxTop,
   };
   const keywordTag = {
     backgroundColor: '#EBEBEB',
-    borderRadius:'27px',
+    borderRadius: '27px',
     width: '80px',
-    fontSize:'14px',
-    fontWeight:'500',
-    font:'bold',
+    fontSize: '14px',
+    fontWeight: '500',
+    font: 'bold',
     marginLeft: '25px',
     marginRight: '25px',
     color: 'black',
@@ -44,35 +45,34 @@ const Header = () => {
     marginBottom: '13px',
   };
 
-
   var keywordBoxRef = useRef(null);
-   useEffect(
-     () => {},
-      window.addEventListener('resize',()=>{
-        setkeyWordBoxLeft(`${keywordBoxRef.current.getBoundingClientRect().x+100}px`);
-        setkeyWordBoxTop(`${keywordBoxRef.current.getBoundingClientRect().y+42}px`);
-      })  
-   );
-  
+  useEffect(
+    () => {},
+    window.addEventListener('resize', () => {
+      setkeyWordBoxLeft(`${keywordBoxRef.current.getBoundingClientRect().x + 100}px`);
+      setkeyWordBoxTop(`${keywordBoxRef.current.getBoundingClientRect().y + 42}px`);
+    }),
+  );
+
   useEffect(() => {
     //로그인&유저정보 state에 저장
     const readme_login = localStorage.getItem('readme_login');
     const readme_userInfo = localStorage.getItem('readme_userInfo');
     if (readme_login && readme_userInfo) {
-      signIn()
+      signIn();
       setUserInfo(readme_userInfo);
       signIn();
     }
-    
-    setkeyWordBoxLeft(`${keywordBoxRef.current.getBoundingClientRect().x+100}px`);
-    setkeyWordBoxTop(`${keywordBoxRef.current.getBoundingClientRect().y+42}px`);
- 
+
+    setkeyWordBoxLeft(`${keywordBoxRef.current.getBoundingClientRect().x + 100}px`);
+    setkeyWordBoxTop(`${keywordBoxRef.current.getBoundingClientRect().y + 42}px`);
+
     //로그인이 되었는지 안되었는지 판단
-    if(location.state!==null){
-      if(location.state.isLoginSuccess===true){
+    if (location.state !== null) {
+      if (location.state.isLoginSuccess === true) {
         signIn();
       }
-    }else{
+    } else {
       signOut();
     }
   }, []);
@@ -93,18 +93,18 @@ const Header = () => {
             style={{ display: 'relative', marginLeft: '18px', paddingTop: '10px' }}
             ref={keywordBoxRef}
             onClick={() => {
-              visible()
+              visible();
             }}
           >
-            <Searchbar/>
+            <Searchbar />
           </div>
         </span>
 
         {/* 로그인시 출력 컴포넌트 */}
         {loginCheck ? (
           <>
-            <Button disabled style={{ color: '#1976d2', marginTop: '43px', fontSize: '23px' }}>
-              {userInfo}님
+            <Button disabled style={{ color: '#1976d2', marginTop: '30px', fontSize: '23px' }}>
+              {JSON.parse(userInfo).name}님
             </Button>
             <Button
               href="/login"
@@ -135,7 +135,7 @@ const Header = () => {
       </div>
 
       {visibleCheck && (
-        <div className="keywordBox" style={keywordBoxLeftRight} >
+        <div className="keywordBox" style={keywordBoxLeftRight}>
           <div style={{ display: 'inlineBlock' }}>
             {dummeyKeywords.map((data, index) => {
               return (
