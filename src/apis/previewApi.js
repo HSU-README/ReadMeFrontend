a56b6ec9f661a90e7ed74b1c662412923ed2731d
yjh-1008 <exesea12@gmail.com>
2022-05-19 03:03:35 +0900

    merge jyounh

--- src/apis/previewApi.js
+++ src/apis/previewApi.js
@@ -10,6 +10,7 @@ const serverApi = axios.create({
 
 export const getPreview = async (docId) => {
   const response = await serverApi.get(`/api/v1/doc/${docId}/preview`);
+  console.log("response :", response)
   try {
     return response.data.result;
   } catch (error) {
