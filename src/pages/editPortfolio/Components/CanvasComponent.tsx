56009818aee9e5c95fc75731bef60841e8cf482d
LINKER <temp2temp@daum.net>
2022-07-27 14:45:15 +0900

    안쓰는 임포트 제거

--- src/pages/editPortfolio/Components/CanvasComponent.tsx
+++ src/pages/editPortfolio/Components/CanvasComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef,useEffect } from "react";
+import React, { useContext, useRef } from "react";
 import { ResizeEnable, Rnd } from "react-rnd";
 import { CanvasContext, ICanvasComponent } from "../CanvasContainer";
 import { resizeHandleClasses } from "../canvasUtils";
